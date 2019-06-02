0d8ac402648d93577dd0189b11a0c32ff1235b07
kuroppe <kuroppems@gmail.com>
2019-06-02 21:59:16 +0900

    vscode以外のディレクトリ下のファイルのシンボリックリンクを作成する

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-DOTDIR="$HOME/dotfiles"
-for dirName in `find $DOTDIR -maxdepth 1 -type d | grep -v "\/\."`; do
-    [ $dirName = $DOTDIR ] && continue
-    for fileName in `find $dirName -maxdepth 1 -type f | grep -v "install.sh"`; do
-        echo $fileName
+DOTDIR=$HOME/dotfiles
+
+for dirPath in `find $DOTDIR -maxdepth 1 -type d | grep -v "\/\."`; do
+    [ $dirPath = $DOTDIR ] && continue
+    [ $dirPath = $DOTDIR/vscode ] && continue
+    for fileName in `find $dirPath -maxdepth 1 -type f | grep -v "install.sh" | awk -F/ '{print $NF}'`; do
+        ln -snfv "$dirPath/$fileName" "$HOME/$fileName"
     done
 done
 # ln -sfnv ~/Library/Mobile\ Documents/com~apple~CloudDocs/ ~/iCloud
