5c0f78d8a3e4f7bdf62683e13b0edfe5c5a720c1
kuroppe <kuroppems@gmail.com>
2019-06-02 22:28:31 +0900

    $HOME -> ~/に変更

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ for dirPath in `find $DOTDIR -maxdepth 1 -type d | grep -v "\/\."`; do
     [ $dirPath = $DOTDIR ] && continue
     [ $dirPath = $DOTDIR/vscode ] && continue
     for fileName in `find $dirPath -maxdepth 1 -type f | grep -v "install.sh" | awk -F/ '{print $NF}'`; do
-        ln -snfv "$dirPath/$fileName" "$HOME/$fileName"
+        ln -snfv $dirPath/$fileName ~/$fileName
     done
 done
 
