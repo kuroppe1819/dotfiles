71b9f25c446fd37e39f1c1ded543ed7907c13b2e
kuroppe <kuroppems@gmail.com>
2019-06-02 22:10:08 +0900

    VSCodeの設定を追加

--- install.sh
+++ install.sh
@@ -10,3 +10,14 @@ for dirPath in `find $DOTDIR -maxdepth 1 -type d | grep -v "\/\."`; do
     done
 done
 # ln -sfnv ~/Library/Mobile\ Documents/com~apple~CloudDocs/ ~/iCloud
+
+
+if [ -e ~/Library/Application\ Support/Code/User ]; then
+    ln -snfv settings.json ~/Library/Application\ Support/Code/User/settings.json
+    
+    for extension in `cat ~/dotfiles/vscode/extensions.txt`; do
+        code --install-extension $extension
+    done
+else 
+    echo "Not found the VSCode"
+fi
