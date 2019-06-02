13386ddb8f2426937e740ae2ca2f224c6556e53e
kuroppe <kuroppems@gmail.com>
2019-06-02 21:22:27 +0900

    dotfiles下のディレクトリ名を全て取得

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
-ln -sfnv ~/Library/Mobile\ Documents/com~apple~CloudDocs/ ~/iCloud
+#!/bin/bash
+
+DOTDIR="$HOME/dotfiles"
+for dirName in `find $DOTDIR -maxdepth 1 -type d | grep -v "\/\."`; do
+    [ $dirName = $DOTDIR ] && continue
+    echo $dirName
+done
+# ln -sfnv ~/Library/Mobile\ Documents/com~apple~CloudDocs/ ~/iCloud
