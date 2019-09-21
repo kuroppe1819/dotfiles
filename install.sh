bb251a1f476a931b0471b980d3615369d6c8fd79
Atsusuke <atsusuke-murata@cybozu.co.jp>
2019-09-22 02:14:56 +0900

    install.sh実行時にvscodeのkeybindingも追加するようにした

--- install.sh
+++ install.sh
@@ -37,6 +37,7 @@ ln -snfv $DOTDIR/vim/colors ~/.vim/colors
 # VSCode settings
 if [ -e ~/Library/Application\ Support/Code/User ]; then
     \cp -f $DOTDIR/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+    \cp -f $DOTDIR/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
     for extension in `cat $DOTDIR/vscode/extensions.txt`; do
         code --install-extension $extension
     done
