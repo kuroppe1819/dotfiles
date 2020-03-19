838287e7baeff569c6a0fc68a9cc50b9a77d692a
kuroppe <kuroppems@gmail.com>
2020-03-20 07:12:06 +0900

    Rename to update_vscode_settings.sh

--- update_vscode_settings.sh
+++ update_vscode_settings.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+DOTDIR=$HOME/dotfiles
 if [ -e ~/Library/Application\ Support/Code/User ]; then
     \cp -f $DOTDIR/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
     \cp -f $DOTDIR/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
