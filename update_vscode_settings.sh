c8b5d60d069ee2a872b268e82e93b3b4fe5db728
kuroppe <kuroppems@gmail.com>
2020-07-24 16:27:28 +0900

    VSCodeの変数展開のスニペットを作成

--- update_vscode_settings.sh
+++ update_vscode_settings.sh
@@ -3,6 +3,7 @@ DOTDIR=$HOME/dotfiles
 if [ -e ~/Library/Application\ Support/Code/User ]; then
     \cp -f $DOTDIR/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
     \cp -f $DOTDIR/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+    \cp -f $DOTDIR/vscode/expand-variable.code-snippets ~/Library/Application\ Support/Code/User/snippets/expand-variable.code-snippets
     for extension in `cat $DOTDIR/vscode/extensions.txt`; do
         code --install-extension $extension
     done
